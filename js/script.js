c2c4bb5e172c9437b8a1f3419503ec36b61cf52c
neootavio <99883591+neootavio@users.noreply.github.com>
2022-08-26 12:06:39 -0300

    atualização js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,48 @@
-function alerta(){
+let menu1 = document.getElementById('menu1');
+
+let tela = document.getElementById('tela');
+
+let painel1 = document.getElementById('painel1');
+
+function alerta() {
 
     alert("Caso recuse seu nível não subirá!");
 
 }
 
-function comecar(){
-    let tela = document.querySelector('.tela');
+function comecar() {
 
-    tela.style.backgroundColor = 'red';
+    voltarTela();
 
-    alert("chegou");
+    painel1.innerHTML = '<button id="botao2" onclick="concluir()">Concluir</button>'
+
+}
+
+function concluir() {
+    alert("Parabéns, XP + 5");
+
+    trocarTela();
+
+    painel1.innerHTML = '<button id="botao1" onclick="comecar()">Começar</button>'
+
+}
+
+function trocarTela() {
+    tela.classList.add('esconder');
+
+    tela.classList.remove('mostrar');
+
+    menu1.classList.add('escondider');
+
+    menu1.classList.remove('mostrar');
+}
+
+function voltarTela() {
+    tela.classList.remove('esconder');
+
+    tela.classList.add('mostrar');
+
+    menu1.classList.remove('escondider');
+
+    menu1.classList.add('mostrar');
 }
