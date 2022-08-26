2849de08a93fefbf29993c4035be8a7cd82368fe
neootavio <99883591+neootavio@users.noreply.github.com>
2022-08-26 11:09:17 -0300

    teste

--- js/script.js
+++ js/script.js
@@ -3,3 +3,11 @@ function alerta(){
     alert("Caso recuse seu nível não subirá!");
 
 }
+
+function comecar(){
+    let tela = document.querySelector('.tela');
+
+    tela.style.backgroundColor = 'red';
+
+    alert("chegou");
+}
