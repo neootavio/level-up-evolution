76f73567d3cedbd6835e6782c14e102b70f0e6b0
neootavio <99883591+neootavio@users.noreply.github.com>
2022-09-04 21:01:50 -0300

    att

--- js/Jogador.js
+++ js/Jogador.js
@@ -2,19 +2,37 @@ export class Jogador {
 
     _nome;
     _level = 1;
-   
-    constructor(nome){
+    _experiencia = 0;
 
-        this._nome = nome;        
+    constructor(nome) {
+
+        this._nome = nome;
 
     }
 
-    get nome(){
+    get nome() {
         return this._nome;
     }
 
-    get level(){
+    set nome(nome) {
+        this._nome = nome;
+    }
+
+    get level() {
         return this._level;
     }
 
+    ganharXP(xp) {
+        this._experiencia += xp;
+        this.levelUp();
+    }
+
+    levelUp() {
+        if (this._experiencia === this._level + 3) {
+
+            this._level++;
+
+        }
+
+    }
 }
--- js/main.js
+++ js/main.js
@@ -1,10 +1,32 @@
 import { Jogador } from "./Jogador.js";
 
-const jogador1 = new Jogador("Otávio");
-
 let nomeTela = document.getElementById('nomeJogador');
 let levelTela = document.getElementById('levelJogador');
 
+let nome = document.getElementById('textoNome');
 
-nomeTela.innerText = jogador1.nome;
-levelTela.innerText += " - " + jogador1.level;
+let botaoNome = document.getElementById('botaoNome');
+
+botaoNome.onclick = cadastraJogador;
+
+botaoConcluir.onclick = concluirQuest;
+
+const jogador = new Jogador(nome.value);
+
+
+function cadastraJogador(){
+
+    jogador.nome = nome.value;
+    console.log(jogador);
+
+    nomeTela.innerText = nome.value;
+    levelTela.innerText += " - " + jogador.level;
+
+}
+
+function concluirQuest(){
+
+    jogador.ganharXP(1);
+    console.log(jogador);
+    
+}
